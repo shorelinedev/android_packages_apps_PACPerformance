17017b9503879d3bad860be723a2d55a4aa764ed
Willi Ye <williye97@gmail.com>
2014-03-20 22:19:07 +0100

    fix building
    PS2: fix derp
    
    Change-Id: Iaf8532c8ebcaa59d073082aa60b0707b8525055e

--- Android.mk
+++ Android.mk
@@ -6,14 +6,16 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13
 
 LOCAL_PACKAGE_NAME := PACPerformance
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
+##################################################
+include $(CLEAR_VARS)
+
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
