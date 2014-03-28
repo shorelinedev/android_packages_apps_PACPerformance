6e82274479e0ee600efe38086d121e4ecdbe267a
Willi Ye <williye97@gmail.com>
2014-03-28 18:12:46 +0100

    do not build PACPerformance in priv-app folder
    Change-Id: I5901fad7ecf2821b792aa2a5e4342bdb4362be56

--- Android.mk
+++ Android.mk
@@ -1,21 +1,15 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 
-LOCAL_PACKAGE_NAME := PACPerformance
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := PACPerformance
 
 include $(BUILD_PACKAGE)
-##################################################
-include $(CLEAR_VARS)
 
-include $(BUILD_MULTI_PREBUILT)
-
-# Use the folloing include to make our test apk.
+# additionally, build tests in sub-folders in a separate .apk
 include $(call all-makefiles-under,$(LOCAL_PATH))
